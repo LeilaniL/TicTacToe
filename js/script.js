f647bca75992617b3ef6a057102732745ac73a39
Leilani Leach and Maxwell Dubin <maxwell@>
2018-11-01 16:43:18 -0700

    Add columns and hover effects

--- js/script.js
+++ js/script.js
@@ -7,24 +7,44 @@ var gameGrid = [
 function isGameOver() {
   for (var x = 0; x < 3; x++) {
     if(gameGrid[x][0] == gameGrid[x][1] && gameGrid[x][1] == gameGrid[x][2] && gameGrid[x][0] != " ") {
-      alert("winner");
-     
+      if (turnIndicator % 2 === 0) {
+        alert("Player 'O' wins!");
+      }
+      else {
+        alert("Player 'X' wins!");
+      }
     }
     if(gameGrid[0][x] == gameGrid[1][x] && gameGrid[1][x] == gameGrid[2][x] && gameGrid[0][x] != " ") {
-      alert("winner");
-    
+      if (turnIndicator % 2 === 0) {
+        alert("Player 'O' wins!");
+      }
+      else {
+        alert("Player 'X' wins!");
+      }
     }
-    if (gameGrid[0][0] == gameGrid[1][1] && gameGrid[1][1] == gameGrid[2][2] && gameGrid[0][0] != " ") {
-      alert("winner");
-     
+  }
+  if (gameGrid[0][0] == gameGrid[1][1] && gameGrid[1][1] == gameGrid[2][2] && gameGrid[0][0] != " ") {
+    if (turnIndicator % 2 === 0) {
+      alert("Player 'O' wins!");
     }
-    if (gameGrid[0][2] == gameGrid[1][1] && gameGrid[1][1] == gameGrid[2][0] && gameGrid[0][2] != " ") {
-      alert("winner");
-    
+    else {
+      alert("Player 'X' wins!");
     }
-    
+  }
+  if (gameGrid[0][2] == gameGrid[1][1] && gameGrid[1][1] == gameGrid[2][0] && gameGrid[0][2] != " ") {
+    if (turnIndicator % 2 === 0) {
+      alert("Player 'O' wins!");
+    }
+    else {
+      alert("Player 'X' wins!");
+    }
   }
 }
+
+
+  
+
+
 
 var turnIndicator = 0;
 
@@ -37,7 +57,9 @@ $(document).ready(function() {
     if (turnIndicator % 2 === 0 && gameGrid[i][j] == " ") {
       gameGrid[i][j] = "X";
       turnIndicator++;
-      $(this).text("X");
+      $(this).html("X");
+      // alert("hi");
+      // debugger;
       isGameOver();
       
     }
@@ -46,10 +68,9 @@ $(document).ready(function() {
       turnIndicator++;
       $(this).text("O");
       isGameOver();
-      
     }
   });
   $("button").click(function(){
       location.reload();
   });
-});
+})
